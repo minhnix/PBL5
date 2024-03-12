6c75343b8317c210308273101ec18e6eec57bc7f
dattrungnam <datvtrg0510@gmail.com>
2024-03-12 23:47:13 +0700

    add relation owner & vehicle

--- src/controller/vehicleController.ts
+++ src/controller/vehicleController.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from "express";
 import { catchError } from "../utils/catchError";
 import { AppDataSource } from "../data-source";
-import { Vehicle } from "../entity";
+import { Owner, Vehicle } from "../entity";
+import { AppError } from "../utils/appError";
 
 let vehicleController = {
   createVehicle: catchError(async (req: Request, res: Response, next) => {
     let vehicleRepository = AppDataSource.getRepository(Vehicle);
-    let vehicle = await vehicleRepository.save(new Vehicle({ ...req.body }));
+    let owner = await AppDataSource.getRepository(Owner).findOneBy({
+      id: req.body.idOwner,
+    });
+    if (!owner) return new AppError("Cannot find owner by that id", 400);
+    req.body.idOwner = undefined;
+    let vehicle = await vehicleRepository.save(
+      new Vehicle({ ...req.body, owner: Promise.resolve(owner) })
+    );
     return res.status(200).json({
       status: "success",
       data: {
@@ -18,6 +26,14 @@ let vehicleController = {
     let vehicleRepository = AppDataSource.getRepository(Vehicle);
     let id = req.params.id;
     let vehicle = await vehicleRepository.findOneBy({ id });
+    if (req.body.idOwner) {
+      let owner = await AppDataSource.getRepository(Owner).findOneBy({
+        id: req.body.idOwner,
+      });
+      req.body.idOwner = undefined;
+      req.body.owner = owner;
+    }
+
     let updateVehicle = await vehicleRepository.save(
       new Vehicle({ ...vehicle, ...req.body })
     );
