300b21531f67967e36c921a92474ffafd9fbb0e0
Minh <bhminh322@gmail.com>
2024-06-10 22:03:15 +0700

    fix: push webhook when crud plate

--- src/controller/middlewareController.ts
+++ src/controller/middlewareController.ts
@@ -1,13 +1,8 @@
-import axios from "axios";
 let middleware = {
   addCreateAt(req, res, next) {
     if (!req.body.createAt) {
       req.body.createdAt = new Date();
     }
-    next();
-  },
-  async handleNotificationVehicle(req, res, next) {
-    const response = await axios.get("http://localhost:5000/webhook");
     next();
   },
 };
